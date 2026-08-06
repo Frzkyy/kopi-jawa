81d194dcc79ab2e78e1843976970174d2a09757c
Frzkyy <fairuzzacky@gmail.com>
2026-08-06 18:52:09 +0700

    aaa

---
+++ kopi-jawa/src/main/java/com/mycompany/kopi/jawa/KopiJawa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.kopi.jawa;
+
+/**
+ *
+ * @author yoxa
+ */
+public class KopiJawa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
