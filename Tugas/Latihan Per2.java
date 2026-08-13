80bd0802550818e7b806c080b8a135a4c01f3e6f
Frzkyy <fairuzzacky@gmail.com>
2026-08-13 16:49:03 +0700

    PER2

--- Tugas/Latihan Per2.java
+++ Tugas/Latihan Per2.java
@@ -1,14 +1,26 @@
 package Tugas;
 
+/*
+WLEEEOEEOEOEOEOEOEOOEO
+*/
+
+import java.util.Scanner;
+
 public class PBO1 {
      public static void main(String[] args) {
           String nama = "Said Fairuz Zacky";
           final long NIM = 25071207048L;
+          char kelas = 'A';
+          String jurusan = "Teknik Informatika (Elektro)";
           int tinggiBadan = 175;
           boolean mahasiswa = true;
 
+          System.out.println("============================================"); //pembatas biar bagus
+
           System.out.println("Nama: " + nama);
           System.out.println("NIM: " + NIM);
+          System.out.println("Kelas: TI-" + kelas);
+          System.out.println("Jurusan: " + jurusan);
           System.out.println("Tinggi Badan: " + tinggiBadan);
           System.out.println("Status Mahasiswa: " + mahasiswa);
 
